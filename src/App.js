0bcbd46041150581022979d23459f0384c03a8f1
Marcus Urani <marcus.urani20@gmail.com>
2021-10-26 21:16:10 -0300

    Criando os primeiros componetes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-function App() {
-  return (
-    <h1>Comprar um pão</h1>
-  );
+import { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
