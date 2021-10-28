5ff1ce6ccfd56cb4a9958fbae0c2e49f0be2a9c6
Marcus Urani <marcus.urani20@gmail.com>
2021-10-27 21:24:56 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import "./components/Assets/index.css"
 import "./components/Assets/App.css"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota = {this.criarNota.bind(this)}/>
+        <ListaDeNotas notas = {this.state.notas}/>
       </section>
     );
   }
