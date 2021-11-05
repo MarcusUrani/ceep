6849165612854403610bd71ce0b7e269378afa11
Marcus Urani <marcus.urani20@gmail.com>
2021-11-05 11:55:04 -0300

    Refatorando e finalizando o projeto

--- src/components/listaDeCategorias/ListaDeCategorias.jsx
+++ src/components/listaDeCategorias/ListaDeCategorias.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class ListaDeCategorias extends Component {
+  constructor() {
+    super();
+    this.state = {categorias: []};
+
+    this._novasCategorias = this._novasCategorias.bind(this);
+  }
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({...this.state, categorias});
+  }
+
   _handlerEventoInput(event) {
     if (event.key == "Enter") {
       let valorCategoria = event.target.value;
@@ -13,7 +31,7 @@ class ListaDeCategorias extends Component {
     return (
       <section className="categorias">
         <ul className="lista-categorias">
-          {this.props.categorias.map((categoria, index) => {
+          {this.state.categorias.map((categoria, index) => {
             return (
               <li key={index} className="lista-categorias__itens">
                 {categoria}
